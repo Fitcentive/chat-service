080d2515595ee7a4fab5c0c94815b2fb5aa85d0f
Vidhyasagar Harihara <vidhyasagar1210@gmail.com>
2022-08-16 20:23:46 -0400

    - Remove heartbeat event

--- lib/chat_web/channels/chat_room_channel.ex
+++ lib/chat_web/channels/chat_room_channel.ex
@@ -27,12 +27,6 @@ defmodule ChatWeb.ChatRoomChannel do
     {:noreply, socket}
   end
 
-  # Heartbeat
-  @impl true
-  def handle_in("heartbeat", _payload, socket) do
-    {:noreply, socket}
-  end
-
   # Channels can be used in a request/response fashion
   # by sending replies to requests from the client
   @impl true
