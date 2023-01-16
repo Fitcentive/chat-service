74c0ebd59a7b87204389be4ae494fbd51efea75b
Vidhyasagar Harihara <vidhyasagar1210@gmail.com>
2023-01-16 15:32:33 -0500

    fix: Update deploy script

--- deploy_scripts/dev_deploy_to_gke.sh
+++ deploy_scripts/dev_deploy_to_gke.sh
@@ -3,8 +3,11 @@
 # Delete old 1.0 image from gcr
 echo "y" | gcloud container images delete gcr.io/fitcentive-dev/chat:1.0 --force-delete-tags
 
-# Build and push image to gcr
+# Remove existing local docker images
 docker image rm gcr.io/fitcentive-dev/chat:1.0
+docker image rm gcr.io/fitcentive-dev/chat
+
+# Build and push image to gcr
 docker build -t gcr.io/fitcentive-dev/chat:latest -t gcr.io/fitcentive-dev/chat:1.0 ../.
 docker push gcr.io/fitcentive-dev/chat:1.0
 
