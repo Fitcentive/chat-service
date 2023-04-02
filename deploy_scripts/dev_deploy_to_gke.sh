15418ec3a80485f9169942390c327c346be7fe73
Vidhyasagar Harihara <vidhyasagar1210@gmail.com>
2023-04-02 18:34:36 -0400

    feat: Change deployment to fitcentive-dev-02 project

--- deploy_scripts/dev_deploy_to_gke.sh
+++ deploy_scripts/dev_deploy_to_gke.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Delete old 1.0 image from gcr
-echo "y" | gcloud container images delete gcr.io/fitcentive-dev/chat:1.0 --force-delete-tags
+echo "y" | gcloud container images delete gcr.io/fitcentive-dev-02/chat:1.0 --force-delete-tags
 
 # Remove existing local docker images
-docker image rm gcr.io/fitcentive-dev/chat:1.0
-docker image rm gcr.io/fitcentive-dev/chat
+docker image rm gcr.io/fitcentive-dev-02/chat:1.0
+docker image rm gcr.io/fitcentive-dev-02/chat
 
 # Build and push image to gcr
-docker build -t gcr.io/fitcentive-dev/chat:latest -t gcr.io/fitcentive-dev/chat:1.0 ../.
-docker push gcr.io/fitcentive-dev/chat:1.0
+docker build -t gcr.io/fitcentive-dev-02/chat:latest -t gcr.io/fitcentive-dev-02/chat:1.0 ../.
+docker push gcr.io/fitcentive-dev-02/chat:1.0
 
 kubectl apply -f deployment/gke-dev-env/
